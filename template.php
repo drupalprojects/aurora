d0f134b665e847774c5db17dbc26bd3fe46a9eac
Sam Richard <snugug@gmail.com>
2012-12-06 12:50:16 -0500

    ASYNC Typekit Awesomeness!

--- template.php
+++ template.php
@@ -217,6 +217,13 @@ function aurora_preprocess_html(&$vars) {
   }
 
   //////////////////////////////
+  // Adds Typekit Kit
+  //////////////////////////////
+  if (theme_get_setting('aurora_typekit_id')) {
+    drupal_add_js('(function(){var e={kitId:"' . theme_get_setting('aurora_typekit_id') . '",scriptTimeout:3e3},t=document.getElementsByTagName("html")[0];t.className+=" wf-loading";var n=setTimeout(function(){t.className=t.className.replace(/(\s|^)wf-loading(\s|$)/g," "),t.className+=" wf-inactive"},e.scriptTimeout),r=document.createElement("script"),i=!1;r.src="//use.typekit.net/"+e.kitId+".js",r.type="text/javascript",r.async="true",r.onload=r.onreadystatechange=function(){var t=this.readyState;if(i||t&&t!="complete"&&t!="loaded")return;i=!0,clearTimeout(n);try{Typekit.load(e)}catch(r){}};var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(r,s)})()', array('type' => 'inline', 'force header' => true));
+  }
+
+  //////////////////////////////
   // LiveReload Integration
   //////////////////////////////
   if (theme_get_setting('aurora_livereload')) {
