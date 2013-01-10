0d11ed88a9298f78c30cfaa7ee05275a71ed1b24
Sam Richard <snugug@gmail.com>
2013-01-09 21:30:42 -0500

    Async Typekit code

--- template.php
+++ template.php
@@ -237,9 +237,7 @@ function aurora_preprocess_html(&$vars) {
   // Add in TypeKit Code.
   //////////////////////////////
   if (theme_get_setting('aurora_typekit_id')) {
-    $typekit_id = theme_get_setting('aurora_typekit_id');
-    drupal_add_js("//use.typekit.net/$typekit_id.js", array('type' => 'external', 'scope' => 'header', 'force header' => TRUE));
-    drupal_add_js('try{Typekit.load();}catch(e){}', array('type' => 'inline', 'scope' => 'header', 'force header' => TRUE));
+    drupal_add_js('(function(){var e={kitId:"' . theme_get_setting('aurora_typekit_id') . '",scriptTimeout:3e3},t=document.getElementsByTagName("html")[0];t.className+=" wf-loading";var n=setTimeout(function(){t.className=t.className.replace(/(\s|^)wf-loading(\s|$)/g," "),t.className+=" wf-inactive"},e.scriptTimeout),r=document.createElement("script"),i=!1;r.src="//use.typekit.net/"+e.kitId+".js",r.type="text/javascript",r.async="true",r.onload=r.onreadystatechange=function(){var t=this.readyState;if(i||t&&t!="complete"&&t!="loaded")return;i=!0,clearTimeout(n);try{Typekit.load(e)}catch(r){}};var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(r,s)})()', array('type' => 'inline', 'force header' => true));
   }
 
 }
