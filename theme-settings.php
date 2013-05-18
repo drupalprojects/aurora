a4e0caac64372e9f572fa63e528f59a758ef3860
Sam Richard <snugug@gmail.com>
2013-05-18 12:19:05 -0700

    Updated to check for new Magic Dev module

--- theme-settings.php
+++ theme-settings.php
@@ -148,7 +148,7 @@ function aurora_form_system_theme_settings_alter(&$form, &$form_state, $form_id 
   // Development
   //////////////////////////////
 
-  if (!module_exists('magic')) {
+  if (!module_exists('magic_dev')) {
     $form['development'] = array(
       '#type' => 'fieldset',
       '#title' => t('Development'),
