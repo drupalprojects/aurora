a1d4a58166a4b7bdfaa0bce127d52529570f6916
schuppo <schuppo@2496198.no-reply.drupal.org>
2013-06-14 17:22:23 +0000

    #2018187: Missing argument for the _aurora_live_reload_settings.

--- theme-settings.php
+++ theme-settings.php
@@ -156,7 +156,7 @@ function aurora_form_system_theme_settings_alter(&$form, &$form_state, $form_id 
       '#weight' => 52
     );
 
-    $form['development'] = array_merge(_aurora_live_reload_settings(), $form['development']);
+    $form['development'] = array_merge(_aurora_live_reload_settings($theme), $form['development']);
 
   }
 
